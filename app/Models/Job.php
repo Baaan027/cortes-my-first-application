3f188ffc6f008ae67f05089e5c385e8e0ae04a71
Ivan Cortes <ivancortes027@gmail.com>
2025-09-20 14:31:19 +0800

    Chapter 3 Initial Commit

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,37 +2,20 @@
 
 namespace App\Models;
 
-class Job
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
 {
-    public static function all()
+    use HasFactory;
+
+    // The table name (we used job_listings)
+    protected $table = 'job_listings';
+
+    protected $fillable = ['employer_id', 'title', 'salary'];
+
+    public function employer()
     {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ],
-        ];
-    }
-
-    public static function find($id)
-    {
-        $job = \Illuminate\Support\Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
+        return $this->belongsTo(Employer::class);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,19 @@
 <?php
 
 namespace Database\Seeders;
-
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Employer;
+use App\Models\Job;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Create 10 employers
+        Employer::factory(10)->create()->each(function ($employer) {
+            Job::factory(5)->create([
+                'employer_id' => $employer->id,
+            ]);
+        });
     }
 }
